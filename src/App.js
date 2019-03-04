512abd0223cd6f24d9318dc1afe85deff056936a
Cory House <housecor@gmail.com>
2019-03-04 17:02:54 -0500

    Remove test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { getCourses, deleteCourse } from "./api/courseApi";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    //test
 
     // Initialize state with an empty course array.
     this.state = {
