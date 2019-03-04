6a362889c28f789dd49dbf59d10b7f1c4a4deb7e
Cory House <housecor@gmail.com>
2019-03-04 14:50:36 -0500

    YOLO

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./App";
 
-render(<div>Hi</div>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
