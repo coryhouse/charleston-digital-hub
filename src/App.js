1d18dd0d136dfecce0acbfd2cd8469e763ef12bb
Cory House <housecor@gmail.com>
2019-03-04 16:34:50 -0500

    Display courses from API call in table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from "react";
+import { getCourses } from "./api/courseApi";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state with an empty course array.
+    this.state = {
+      courses: []
+    };
+  }
+
+  componentDidMount() {
+    getCourses().then(courses => {
+      this.setState({ courses });
+    });
+  }
+
   render() {
-    return <h1>App</h1>;
+    return (
+      <>
+        <h1>App</h1>
+        <h2>Courses</h2>
+        <table>
+          <thead>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+          </thead>
+          <tbody>
+            {this.state.courses.map(course => (
+              <tr>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
