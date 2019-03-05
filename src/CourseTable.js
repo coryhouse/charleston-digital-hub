e33a3e636adfa59a43c562128781f413848b48e4
Cory House <housecor@gmail.com>
2019-03-05 11:28:56 -0500

    Convert to func component, adding propType validation

--- src/CourseTable.js
+++ src/CourseTable.js
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { course } from "./propTypes";
 
-class CourseTable extends Component {
-  render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th />
-            <th>Title</th>
-            <th>Author</th>
-            <th>Category</th>
+const CourseTable = ({ courses, onClickDelete }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th />
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {courses.map(course => (
+          <tr key={course.id}>
+            <td>
+              <button onClick={event => onClickDelete(course.id)}>
+                Delete
+              </button>
+            </td>
+            <td>{course.title}</td>
+            <td>{course.authorId}</td>
+            <td>{course.category}</td>
           </tr>
-        </thead>
-        <tbody>
-          {this.props.courses.map(course => (
-            <tr key={course.id}>
-              <td>
-                <button onClick={event => this.props.onClickDelete(course.id)}>
-                  Delete
-                </button>
-              </td>
-              <td>{course.title}</td>
-              <td>{course.authorId}</td>
-              <td>{course.category}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 CourseTable.propTypes = {
-  courses: PropTypes.arrayOf(course).isRequired,
+  courses: PropTypes.arrayOf(course),
   onClickDelete: PropTypes.func.isRequired
+};
+
+CourseTable.defaultProps = {
+  courses: []
 };
 
 export default CourseTable;
