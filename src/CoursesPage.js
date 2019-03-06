14e53030e8ee460718973625940c9c6a72f7d31f
Cory House <housecor@gmail.com>
2019-03-06 13:24:32 -0500

    HOOOOKKS!

--- src/CoursesPage.js
+++ src/CoursesPage.js
@@ -1,43 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import CourseTable from "./CourseTable";
 import { Redirect } from "react-router-dom";
 import { course } from "./propTypes";
 
-class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
+function CoursesPage(props) {
+  const [redirectToAddCourse, setRedirectToAddCourse] = useState(false);
 
-    // Initialize state with an empty course array.
-    this.state = {
-      redirectToAddCourse: false
-    };
-
-    // Bind in constructor
-    // this.handleDelete = this.handleDelete.bind(this);
+  function handleClickAddCourse() {
+    setRedirectToAddCourse(true);
   }
 
-  handleClickAddCourse = () => {
-    this.setState({ redirectToAddCourse: true });
-  };
-
-  render() {
-    return (
-      <>
-        {this.state.redirectToAddCourse && <Redirect to="/course" />}
-        <h1>Courses</h1>
-        <button onClick={this.handleClickAddCourse}>Add Course</button>
-        {this.props.courses.length === 0 ? (
-          <p>No courses :(</p>
-        ) : (
-          <CourseTable
-            courses={this.props.courses}
-            onClickDelete={this.props.onDelete}
-          />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {redirectToAddCourse && <Redirect to="/course" />}
+      <h1>Courses</h1>
+      <button onClick={handleClickAddCourse}>Add Course</button>
+      {props.courses.length === 0 ? (
+        <p>No courses :(</p>
+      ) : (
+        <CourseTable courses={props.courses} onClickDelete={props.onDelete} />
+      )}
+    </>
+  );
 }
 
 CoursesPage.propTypes = {
