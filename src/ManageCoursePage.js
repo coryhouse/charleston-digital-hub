e20302ecb4938eea326233f8a5832934e7e75ef6
Cory House <housecor@gmail.com>
2019-03-06 14:13:47 -0500

    Switch to HOOOOOKS!

--- src/ManageCoursePage.js
+++ src/ManageCoursePage.js
@@ -1,56 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import TextInput from "./TextInput";
 import { toast } from "react-toastify";
 import Spinner from "./Spinner";
 
-class ManageCoursePage extends Component {
-  state = {
-    course: {
-      id: null,
-      title: "",
-      category: "",
-      slug: "",
-      authorId: ""
-    },
-    errors: {}
-  };
+function ManageCoursePage(props) {
+  const [course, setCourse] = useState({
+    id: null,
+    title: "",
+    category: "",
+    slug: "",
+    authorId: ""
+  });
 
-  static propTypes = {
-    history: PropTypes.object.isRequired,
-    onSave: PropTypes.func.isRequired
-  };
+  const [errors, setErrors] = useState({});
 
-  componentDidMount() {
-    const slug = this.props.match.params.slug;
-    if (slug && this.props.courses.length > 0) {
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug && props.courses.length > 0) {
       // So we're editing an existing course
-      const course = this.props.courses.find(course => course.slug === slug);
-      if (!course) this.props.history.push("/404");
-      this.setState({ course: { ...course } });
+      const course = props.courses.find(course => course.slug === slug);
+      if (!course) props.history.push("/404");
+      setCourse({ ...course });
     }
-  }
+  }, []); // Empty dependency array means this will run only once, when component mounts.
 
-  handleChange = event => {
-    const course = { ...this.state.course };
+  const handleChange = event => {
+    const _course = { ...course };
     // computed property syntax
-    course[event.target.name] = event.target.value;
-    this.setState({ course });
+    _course[event.target.name] = event.target.value;
+    setCourse(_course);
   };
 
-  handleSubmit = event => {
-    const { course } = this.state;
+  const handleSubmit = event => {
     event.preventDefault();
-    const errors = {};
+    const _errors = {};
 
-    if (!course.title) errors.title = "Title is required.";
-    if (!course.category) errors.category = "Category is required.";
+    if (!course.title) _errors.title = "Title is required.";
+    if (!course.category) _errors.category = "Category is required.";
     if (!course.authorId || isNaN(course.authorId))
-      errors.authorId = "Author ID must be a number.";
+      _errors.authorId = "Author ID must be a number.";
 
     // Is there at least one property on the errors object? If so, validation failed.
-    if (Object.keys(errors).length > 0) {
-      this.setState({ errors });
+    if (Object.keys(_errors).length > 0) {
+      setErrors(_errors);
       return;
     }
 
@@ -59,56 +52,58 @@ class ManageCoursePage extends Component {
       authorId: parseInt(course.authorId, 10)
     };
 
-    this.props
+    props
       .onSave(newCourse)
       .then(() => {
         toast.success("🦄Course saved!");
-        this.props.history.push("/courses");
+        props.history.push("/courses");
       })
       .catch(error => {
         toast.error("Oops! Save failed. Please try again.  ¯\\_(ツ)_/¯");
       });
   };
 
-  render() {
-    const { course, errors } = this.state;
-    if (this.props.courses.length === 0) return <Spinner />;
-    return (
-      <div>
-        <h1>Manage Course</h1>
-        <form onSubmit={this.handleSubmit}>
-          <TextInput
-            label="Title"
-            id="title"
-            name="title"
-            onChange={this.handleChange}
-            value={course.title}
-            error={errors.title}
-          />
+  if (props.courses.length === 0) return <Spinner />;
+  return (
+    <div>
+      <h1>Manage Course</h1>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          label="Title"
+          id="title"
+          name="title"
+          onChange={handleChange}
+          value={course.title}
+          error={errors.title}
+        />
 
-          <TextInput
-            label="Category"
-            id="category"
-            name="category"
-            onChange={this.handleChange}
-            value={course.category}
-            error={errors.category}
-          />
+        <TextInput
+          label="Category"
+          id="category"
+          name="category"
+          onChange={handleChange}
+          value={course.category}
+          error={errors.category}
+        />
 
-          <TextInput
-            label="Author"
-            id="author"
-            name="authorId"
-            onChange={this.handleChange}
-            value={course.authorId}
-            error={errors.authorId}
-          />
+        <TextInput
+          label="Author"
+          id="author"
+          name="authorId"
+          onChange={handleChange}
+          value={course.authorId}
+          error={errors.authorId}
+        />
 
-          <button type="submit">Save Course</button>
-        </form>
-      </div>
-    );
-  }
+        <button type="submit">Save Course</button>
+      </form>
+    </div>
+  );
 }
+
+ManageCoursePage.propTypes = {
+  history: PropTypes.object.isRequired,
+  onSave: PropTypes.func.isRequired
+};
 
 export default ManageCoursePage;
