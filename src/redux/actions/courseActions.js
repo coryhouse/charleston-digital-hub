dfeff04f1f1d157d17e984c6ad17d3f6cecf240b
Cory House <housecor@gmail.com>
2019-03-07 11:05:05 -0500

    Add actions and reducers

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -7,11 +7,29 @@ function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
 // Our first thunk. We'll call this from our React component
 export function loadCourses() {
   return function(dispatch) {
     return courseApi.getCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id
+        ? dispatch(updateCourseSuccess(savedCourse))
+        : dispatch(createCourseSuccess(savedCourse));
     });
   };
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,6 +4,12 @@ export default function courseReducer(state = [], action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return [...action.courses];
+    case types.CREATE_COURSE_SUCCESS:
+      return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course
+      );
     default:
       return state;
   }
